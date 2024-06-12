6e340629c50410bdd38264f0ae5415b6db95cdd6
ashishbct <ashish.vishwakarma@gravityilabs.com>
2024-06-12 15:25:57 +0530

    Logical done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,44 @@
-import { FaShoppingCart } from "react-icons/fa";
+import {FaShoppingCart} from "react-icons/fa"
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+
+  const {cart} = useSelector((state) => state);
+
   return (
-    <div className="bg-gray-800 text-white fixed inset w-full">
-      <div className="flex flex-row justify-between mx-5">
+    <div >
+      <nav className="flex justify-between items-center h-20 max-w-6xl mx-auto">
+
         <NavLink to="/">
-          <div className="">
-            <img src="https://picsum.photos/200/300" width={30} height={30} />
+          <div className="ml-5">
+          <img src="../logo.png" className="h-14"/>
           </div>
         </NavLink>
 
-        <div className="flex flex-row gap-5">
-          <NavLink to="/">
-            <p>Home</p>
-          </NavLink>
+          <div className="flex items-center font-medium text-slate-100 mr-5 space-x-6">
+            <NavLink to="/">
+              <p>Home</p>
+            </NavLink>
 
-          <NavLink to="/cart">
-            <div>
-              <FaShoppingCart />
-            </div>
-          </NavLink>
-        </div>
-      </div>
+            <NavLink to="/cart">
+              <div className="relative">
+                  <FaShoppingCart className="text-2xl"/>
+                  {
+                    cart.length > 0 &&
+                    <span
+                    className="absolute -top-1 -right-2 bg-green-600 text-xs w-5 h-5 flex 
+                    justify-center items-center animate-bounce rounded-full text-white" 
+                    >{cart.length}</span>
+                  }
+                  
+              </div>
+            </NavLink>
+            
+          </div>
+      </nav>
     </div>
-  );
+  )
 };
 
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,37 +10,39 @@ const Home = () => {
   async function fetchProductData() {
     setLoading(true);
 
-    try {
+    try{
       const res = await fetch(API_URL);
       const data = await res.json();
 
       setPosts(data);
-    } catch (error) {
+    }
+    catch(error) {
       console.log("Error aagya ji");
       setPosts([]);
     }
     setLoading(false);
   }
 
-  useEffect(() => {
+  useEffect( () => {
     fetchProductData();
-  }, []);
+  },[])
 
   return (
     <div>
-      {loading ? (
-        <Spinner />
-      ) : posts.length > 0 ? (
-        <div>
-          {posts.map((post) => (
-            <Product key={post.id} post={post} />
-          ))}
-        </div>
-      ) : (
-        <div>
+      {
+        loading ? <Spinner />  :
+        posts.length > 0 ? 
+        (<div className="grid  xs:gridcols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
+          {
+            posts.map( (post) => (
+            <Product key = {post.id} post={post}/>
+          ) )
+          }
+        </div>) :
+        <div className="flex justify-center items-center">
           <p>No Data Found</p>
-        </div>
-      )}
+        </div> 
+      }
     </div>
   );
 };
